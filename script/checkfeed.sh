601088cb87bc80ee621519dd849e8f587291ac1f
Denis Renning <denis@devtty.de>
2021-06-08 13:15:32 +0200

    empty

--- script/checkfeed.sh
+++ script/checkfeed.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 
+chk_follower_count () {
+    last_progress=($(sqlite3 -separator " " social.sqlite "SELECT followers, created FROM igaccount_progress WHERE profile_id=1 ORDER BY created DESC LIMIT 1"))
+    echo "followers: ${last_progress[0]}"
+    echo "created: ${last_progress[1]}"
+
+    #compare with old value and update if it is outdated by 14days
+    if [ $(date --date="${last_progress[1]} +14 day" +%s) -le $(date +%s) ]; then
+	echo "Progress data is outdated."
+	echo "How many posts?"
+	read act_posts
+	echo "How many are following you?"
+	read act_followers
+	echo $((act_followers-followers))
+	echo "How many are you following?"
+	read act_following
+
+	$(sqlite3 social.sqlite "INSERT INTO igaccount_progress VALUES (1, $act_followers, $act_following, $act_posts, CURRENT_TIMESTAMP);")
+    fi
+
+    #compare with available data
+    
+}
+
 if [[ -z $1 ]]
 then
     echo "no account name given"
     account_total=$(sqlite3 social.sqlite "SELECT count(*) FROM igaccount")
     followers=$(sqlite3 social.sqlite "SELECT count(*) FROM igaccount i INNER JOIN igaccount_activity ia ON ia.profile_id = i.id WHERE ia.follows = 1") 
     echo "Total account: $account_total"
-    echo "Follower: $followers"    
+    echo "Follower: $followers"
+    chk_follower_count
 else
     exists=$(sqlite3 social.sqlite "SELECT id from igaccount where name = '$1';")
     
